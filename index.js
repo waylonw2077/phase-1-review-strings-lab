ed3683f0eec3b672e3a5061a81e29071ec83405c
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-20 16:39:39 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Waylon'
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser +'!'
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+let firstInitial = currentUser[0]
+let shortGreeting = `Welcome, ${firstInitial}!`
